6b62d1b8738881a2f222164e8a209e5e0f9106f4
Charles Duan <cduan@sbf5.com>
2018-12-16 22:36:11 -0500

    Improved size and gravity computations

--- lib/photobook/positioner.rb
+++ lib/photobook/positioner.rb
@@ -57,55 +57,139 @@ class Photobook
     end
 
     #
-    # Computes the relevant box size given a containing box's dimensions, the
-    # direction of this box (:horiz or :vert), and given parameters. The
-    # argument +index+ is the position of the box being computed relative to its
-    # sibling boxes; +box_count+ is the total number of boxes.
+    # Computes and allocates +total_size+ across the items in +allocations+,
+    # which should be only ItemAllocation objects.
     #
-    # Only the dimension that changes is returned (so for :horiz the height is
-    # returned, and for :vert the width is returned).
-    #
-    def box_size(params, total_size, spacing, index, box_count)
-      total_size -= spacing * (box_count - 1)
-
+    def allocate_sizes(params, allocations, total_size)
       sizes = params['sizes']
-      return total_size / box_count.to_f unless sizes
+      sizes ||= [ 1.0 / allocations.count ] * allocations.count
 
-      unless sizes.is_a?(Array) && sizes.count == box_count
+      unless sizes.is_a?(Array) && sizes.count == allocations.count
         raise ArgumentError, "Sizes parameter is not an array of correct size"
       end
-      sizes.last += 1.0 - sizes.inject(:+)
+      sizes[-1] += 1.0 - sizes.inject(:+)
       unless sizes.all? { |x| x >= 0.0 && x <= 1.0 }
         raise ArgumentError, "Sizes must be between 0 and 1"
       end
 
-      return sizes[index] * total_size
-
+      allocations.zip(sizes).each do |alloc, size|
+        alloc.size = size * total_size
+      end
     end
 
     #
     # Computes the gravity for the current box, based on the 'gravity' parameter
     # given. If it is 'outer' then the gravity is distributed across
     #
-    def compute_gravity(params, index, box_count)
+    def allocate_gravity(params, allocations)
       g = params['gravity']
-      distrib_gravity = (box_count == 1 ? 0.5 : index.to_f / (box_count - 1))
 
-      case g
-      when nil then return 0.5
-      when Numeric then return [ 0, g, 1 ].sort[1]
-      when 'left', 'top' then return 0
-      when 'right', 'bottom' then return 1
-      when 'outer' then return distrib_gravity
-      when 'center' then return 1 - distrib_gravity
-      else return 0.5
+      allocations.each_index do |i|
+        next unless allocations[i].is_a?(ItemAllocation)
+        allocations[i].gravity =
+          case g
+          when nil then 0.5
+          when Numeric then [ 0, g, 1 ].sort[1]
+          when 'left', 'top' then 0
+          when 'right', 'bottom' then 1
+          when 'outer' then distrib_gravity(allocations, i)
+          when 'center' then 1 - distrib_gravity(allocations, i)
+          else 0.5
+          end
       end
     end
 
+    def distrib_gravity(allocations, i)
+      pre_size = allocations[0...i].map(&:size).inject(0.0, :+)
+      post_size = allocations[(i+1)..-1].map(&:size).inject(0.0, :+)
+      return pre_size.to_f / (pre_size + post_size)
+    end
 
     def grid(spec, width, height)
       rows, cols = spec.split(/\s*x\s*/).map(&:to_i)
       boxes([ cols ] * rows, width, height)
+    end
+
+    class SpaceAllocation
+      def initialize(positioner, size)
+        @positioner = positioner
+        @size = size
+      end
+      attr_accessor :size
+
+      def apply(dir, other_size, other_gravity)
+        @positioner.space(@size, dir)
+      end
+    end
+
+    class ItemAllocation
+      def initialize(positioner, box)
+        @positioner = positioner
+        @box = box
+      end
+      attr_accessor :size, :gravity
+      def apply(dir, other_size, other_gravity)
+        if dir == :horiz
+          if @box.is_a?(Array)
+            @positioner.boxes(@box, @size, other_size, :vert, @gravity)
+          else
+            @positioner.photo_box(
+              @positioner.next_photo(), @size, other_size,
+              @gravity, other_gravity
+            )
+          end
+        else
+          if @box.is_a?(Array)
+            @positioner.boxes(@box, other_size, @size, :horiz, @gravity)
+          else
+            @positioner.photo_box(
+              @positioner.next_photo(), other_size, @size,
+              other_gravity, @gravity
+            )
+          end
+        end
+      end
+    end
+
+    def allocate(params, total_size, spec)
+      total_spacing = 0.0
+      total_items = 0
+      default_spacing = spacing_size(params)
+      allocations = []
+      items = []
+
+      #
+      # First, determine the order of items to be placed within the box.
+      spec.each do |item|
+        case item
+        when Integer, Array, true
+          # For photos (true) and sub-boxes, insert a default spacer if the last
+          # allocation was for an item. Then create the allocation for the item.
+          if allocations.last.is_a?(ItemAllocation)
+            total_spacing += default_spacing
+            allocations.push(SpaceAllocation.new(self, default_spacing))
+          end
+          item = [ true ] * item if item.is_a?(Integer)
+          alloc = ItemAllocation.new(self, item)
+          allocations.push(alloc)
+          items.push(alloc)
+          total_items += 1
+
+        when Numeric
+          # For spacers, insert the spacer and update the total space.
+          this_space = rel_to_dim(item)
+          total_spacing += this_space
+          allocations.push(SpaceAllocation.new(self, this_space))
+        else
+          raise "Unknown boxes item #{item.class}: #{item}"
+        end
+      end
+
+      # Next, compute the space left and allocate it across the items.
+      allocate_sizes(params, items, total_size - total_spacing)
+
+      # Finally, allocate the gravity.
+      allocate_gravity(params, allocations)
     end
 
     #
@@ -118,41 +202,13 @@ class Photobook
       box(direction, width, height) do
         first = true
         spec = spec.reject { |x| x.is_a?(Hash) }
-        spec.each_with_index do |item, index|
-          space(spacing, direction) unless first
-          first = false
+        allocations = allocate(
+          params, direction == :vert ? height : width, spec
+        )
 
-          this_gravity = compute_gravity(params, index, spec.count)
-
-          #
-          # For a vertical box, the current gravity will position boxes
-          # vertically and the inherited gravity will position them
-          # horizontally. The opposite is true for a horizontal box.
-          #
-          if direction == :vert
-            this_width, next_dir = width, :horiz
-            this_height = box_size(params, height, spacing, index, spec.count)
-            horiz_gravity, vert_gravity = last_gravity, this_gravity
-          else
-            this_height, next_dir = height, :vert
-            this_width = box_size(params, width, spacing, index, spec.count)
-            horiz_gravity, vert_gravity = this_gravity, last_gravity
-          end
-
-          item = [ true ] * item if item.is_a?(Integer)
-          case item
-          when Array
-            boxes(item, this_width, this_height, next_dir, this_gravity)
-          when true
-            photo_box(
-              next_photo(), this_width, this_height,
-              horiz_gravity, vert_gravity
-            )
-          when Numeric
-            space(rel_to_dim(item))
-          else
-            raise "Unknown boxes item #{item}"
-          end
+        other_size = (direction == :vert ? width : height)
+        allocations.each do |item|
+          item.apply(direction, other_size, last_gravity)
         end
       end
     end
